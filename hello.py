b3157956adf838bc62fb1dea34296ce1de9feb6e
Ugonna <michealdaniels58@hotmail.com>
2021-06-29 11:45:50 +0100

    types  and conditions

--- hello.py
+++ hello.py
@@ -2,5 +2,17 @@ print ("Hello, world!")
 
 name = input("Name: ")
 
-print ("Hello " + name)
+print ("Hello, " + name)
 print (f"Hello, {name}")
+
+name = input("Name: ")
+
+n = input ("Number: ")
+if n > 0:
+    print("n is positive")
+elif n < 0:
+    print("n is negative")
+else:
+    print("n is Zero")
+
+m = input("Are you done? Y/N")
