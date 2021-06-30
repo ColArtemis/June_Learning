dc21e644002aeb28c96be7c1badf03e05bfb5297
Ugonna <michealdaniels58@hotmail.com>
2021-06-30 09:30:49 +0100

    update 30th june

--- hello.py
+++ hello.py
@@ -5,9 +5,7 @@ name = input("Name: ")
 print ("Hello, " + name)
 print (f"Hello, {name}")
 
-name = input("Name: ")
-
-n = input ("Number: ")
+n = int (input ("Number: "))
 if n > 0:
     print("n is positive")
 elif n < 0:
@@ -15,4 +13,6 @@ elif n < 0:
 else:
     print("n is Zero")
 
-m = input("Are you done? Y/N")
+m = input("Are you done? Y/N: ")
+
+name = "Harry"
