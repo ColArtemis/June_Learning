af7a3ef9c39d707b41276f81a8e41d9497818005
Ugonna <michealdaniels58@hotmail.com>
2021-07-01 09:31:42 +0100

    Last commit before i switch to VS Code

--- hello.py
+++ hello.py
@@ -1,11 +1,13 @@
+#The first code a simple Hello
 print ("Hello, world!")
-
+#input function
 name = input("Name: ")
 
 print ("Hello, " + name)
+#formatted strings
 print (f"Hello, {name}")
-
-n = int (input ("Number: "))
+#conditions
+n = int (input ("Number: ")) #make sure to call an integer or you'll have an error
 if n > 0:
     print("n is positive")
 elif n < 0:
@@ -14,5 +16,16 @@ else:
     print("n is Zero")
 
 m = input("Are you done? Y/N: ")
-
+#sequencing
 name = "Harry"
+
+print(name[0])
+
+name2 = ["Harry", "Hermione", "Ron"]
+print(name2)
+print(name2[0])
+m = input("Are you done? Y/N: ")
+
+coordinatex = 10.0
+coordinatey = 20.0
+coordinate = (10.0, 20.0)
